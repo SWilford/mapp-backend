f321451c8cfaa437572eb404414c57618ed12d54
Spencer <sawilford2006@gmail.com>
2024-09-16 14:28:17 -0400

    basic server setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Test running')
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
